3d53c18fd2f5a636033abcd535ca9f983b4e9603
Barbara <muesingb@gmail.com>
2019-11-05 15:19:49 -0500

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+10.times do |t|
+SchoolClass.create(title: Faker::ProgrammingLanguage.name, room_number: Faker::Number.number(digits: 3))
+end
+
+10.times do |t|
+Student.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name)
+end
